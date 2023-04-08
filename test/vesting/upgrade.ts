7829cd5331510b6b7bd38ae7fac17209400c7973
zbirenbaum <zacharyobirenbaum@gmail.com>
2023-04-08 17:47:27 -0400

    add vesting upgrader

---
+++ test/vesting/upgrade.ts
@@ -0,0 +1,32 @@
+import '@nomiclabs/hardhat-ethers';
+import '@nomiclabs/hardhat-etherscan'
+import '@openzeppelin/hardhat-upgrades';
+import hre, {ethers, upgrades} from 'hardhat';
+import {
+  formatAmount,
+} from '../utils/helpers';
+import { adminAddress, CONTRACTS as c } from '../utils/constants';
+
+
+const netName = hre.network.name;
+const getSigner = async () => {
+  if(netName == 'hardhat') {
+    const signer = await ethers.getImpersonatedSigner(adminAddress);
+    const [ethWallet] = await ethers.getSigners();
+    await ethWallet.sendTransaction({
+      to: signer.address,
+      value: ethers.utils.parseEther("1.0"),
+    });
+    return signer
+  }
+  const [signer] = await ethers.getSigners();
+  return signer;
+}
+
+export async function vestingFixture () {
+  const signer = await getSigner();
+  const VesterV2 = await ethers.getContractFactory('VesterV2', signer);
+  console.log('upgrading vester');
+  await upgrades.upgradeProxy(c.vTND, VesterV2);
+  console.log('upgraded vester');
+}
