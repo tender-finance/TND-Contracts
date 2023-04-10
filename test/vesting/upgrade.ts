b35c928a93c1214580f55f395292fa4821e3c933
zbirenbaum <zacharyobirenbaum@gmail.com>
2023-04-10 16:08:59 -0400

    impl verification

--- test/vesting/upgrade.ts
+++ test/vesting/upgrade.ts
@@ -3,6 +3,7 @@ import '@nomiclabs/hardhat-etherscan'
 import '@openzeppelin/hardhat-upgrades';
 import hre, {ethers, upgrades} from 'hardhat';
 import { adminAddress, CONTRACTS as c } from '../utils/constants';
+import { getImplementationAddress } from '@openzeppelin/upgrades-core';
 
 
 const netName = hre.network.name;
@@ -20,9 +21,10 @@ const getSigner = async () => {
   return signer;
 }
 
-async function verify () {
+async function verifyImplementation (contract: any) {
+  const impl = await getImplementationAddress(hre.network.provider, contract);
   await hre.run("verify:verify", {
-    address: '0x438BE5cBFAfDC89abf15B9565842cDbe43382dB0',
+    address: impl,
     contract: 'contracts/staking/VesterV2.sol:VesterV2',
     constructorArguments: [],
   });
@@ -32,9 +34,7 @@ async function upgradeVester() {
   const VesterV2 = await ethers.getContractFactory('VesterV2', signer);
   console.log('upgrading vester');
   const vTND = await upgrades.upgradeProxy(c.vTND, VesterV2);
-  await hre.run("verify:verify", {
-    address: vTND.address,
-  });
+  await verifyImplementation(vTND);
   console.log('upgraded vester');
 }
 
